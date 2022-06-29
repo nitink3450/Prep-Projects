dac0708a60918c42bfc3f5e5c2d25ae761bece22
nitin k <nitinkanhurkar2016@gmail.com>
2022-06-30 01:55:40 +0530

    29june22e2

--- Digital Clock/clock.js
+++ Digital Clock/clock.js
@@ -1,4 +1,23 @@
-//set alarm button-
+//DIGITAL CLOCK FUNCTION-
+const Digital_Clock = () => {
+  const time =new Date();
+  let hrs = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  
+  let meridiem = document.getElementById('meridiem');
+  let hour = hrs;
+  let ampm = (hour >= 12) ? ('PM') : ('AM');
+  meridiem.innerText= ampm;
+   
+  document.getElementById('hour').innerText = hour;
+  document.getElementById('minutes').innerText = min;
+  document.getElementById('seconds').innerText = sec;
+   
+}
+setInterval(Digital_Clock,1000);
+
+//SET ALARM BUTTON REPLACING TEXTS-
 let button = document.getElementById('btn1');
 button.addEventListener('click', () => {
     button.innerHTML = 'Party Time !!';
@@ -8,71 +27,60 @@ button.addEventListener('mouseleave', () => {
     button.innerHTML = 'Set Alarm';
 });
 
-//Clock Time Display function-
-const Display = () =>{
+//AUTOMATIC GRETTINGS-
+const date = new Date();
+let hrs = date.getHours();
+let wakeUp = parseInt(document.getElementById("wake").value);
+let lunchUp = parseInt(document.getElementById("lunch").value);
+let napUp = parseInt(document.getElementById("nap").value);
+let nightUp = parseInt(document.getElementById("night").value);
 
-    const date =new Date();//accesssed global 24 hrs time
-    let hour = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let ampm = document.getElementById('ampm');
-
-    // From 1 to 12 = AM And From 12 to 24 = PM 
-    let finalhrs = hour ;
-    finalhrs >= 12 ? (ampm.innnerText = 'PM') : (ampm.innnerText = 'AM');
-    // 24 hrs Time Converted into 12 hrs(easy to track time)
-    finalhrs > 12 ? (finalhrs -= 12) : ((finalhrs = 0) ? (finalhrs = 12) : (finalhrs));
-
-    // accessed time gave to UI clock id sections
-    document.getElementById('hour').innerHTML = finalhrs ;
-    document.getElementById('minutes').innerHTML = minutes;
-    document.getElementById('seconds').innerText = seconds;
+if (hrs <= 12) 
+{
+  document.getElementById("morning-label-id").innerHTML = "Good Morning!";
 }
-setInterval(Display, 1000);
+if (hrs >= 12 && hrs < 16) 
+{
+  document.getElementById("morning-label-id").innerHTML = "Good Afternoon!";
+}
+if (hrs >= 16 && hrs < 19)
+ {
+  document.getElementById("morning-label-id").innerHTML = "Good Evening";
+}
+if (hrs >= 19 && hrs <= 23) 
+{
+  document.getElementById("morning-label-id").innerHTML = "Good Night";
+}
 
-function clicked() {
+//AFTER BUTTON PRESSED/SETTING ALARM-
+function clicked(){
+  
+  //TO SET TIMING-
+  let timing = document.getElementsByClassName("sets");
+  timing[0].innerText ="Wake Up Time:" + " " + wake.options[wake.selectedIndex].text;
+  timing[1].innerText ="Lunch Time:" + " " + lunch.options[lunch.selectedIndex].text;
+  timing[2].innerText ="Nap Time:" + " " + nap.options[nap.selectedIndex].text;
+  timing[3].innerText ="Night Time:" + " " + night.options[night.selectedIndex].text;
 
-  var utha = document.getElementById("wake").value;
-  var hours = new Date().getHours();
-  var jeva = document.getElementById("lunch").value;
-  var chaha = document.getElementById("nap").value;
-  var jhopa = document.getElementById("night").value;
-  if (parseInt(utha) === hours) {
+  //TO DISPLAY MESSAGE AND IMAGE ON TIME-
+  if(wakeUp === hrs)
+  {
     document.getElementById("image_photo").src = "./curtons-img.svg";
     document.getElementById("breakfast-label-id").innerHTML ="GRAB SOME HEALTHY BREAKFAST!!!";
-    document.getElementById("morning-label-id").innerHTML = "GOOD MORNING!!";
   }
-  if (parseInt(jeva) === hours) {
+  if(lunchUp === hrs)
+  {
     document.getElementById("image_photo").src = "./pizza.svg"
     document.getElementById("breakfast-label-id").innerHTML ="LETS HAVE SOME LUNCH !!";
-    document.getElementById("morning-label-id").innerHTML = "GOOD AFTERNOON!!";
   }
-  if (parseInt(chaha) === hours) {
+  if(napUp === hrs)
+  {
     document.getElementById("image_photo").src = "./lunch_image.png"
     document.getElementById("breakfast-label-id").innerHTML ="GET SOME TEA";
-    document.getElementById("morning-label-id").innerHTML = "GOOD EVENING!!";
   }
-  if (parseInt(jhopa) === hours) {
+  if(nightUp === hrs)
+  {
     document.getElementById("image_photo").src = "./goodnight_image.svg"
     document.getElementById("breakfast-label-id").innerHTML = "TIME TO BED";
-    document.getElementById("morning-label-id").innerHTML = "GOOD NIGHT!!";
   }
-
-  var container = document.createElement("p");
-  document.getElementsByClassName("time-schedule")[0].appendChild(container);
-  var timing = document.getElementsByClassName("setting");
-  var item1 = document.getElementById("wake");
-  var item2 = document.getElementById("lunch");
-  var item3 = document.getElementById("nap");
-  var item4 = document.getElementById("night");
-
-  timing[0].innerText =
-    "Wake Up Time:" + " " + item1.options[item1.selectedIndex].text;
-  timing[1].innerText =
-    "Lunch Time:" + " " + item2.options[item2.selectedIndex].text;
-  timing[2].innerText =
-    "Nap Time:" + " " + item3.options[item3.selectedIndex].text;
-  timing[3].innerText =
-    "Night Time:" + " " + item4.options[item4.selectedIndex].text;
-
-}
+ }
