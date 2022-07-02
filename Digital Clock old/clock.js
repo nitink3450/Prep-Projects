780e40a7e68787b6ccd360b9a079048162a5143e
nitin k <nitinkanhurkar2016@gmail.com>
2022-07-02 15:14:28 +0530

    2july22e2

---
+++ Digital Clock old/clock.js
@@ -0,0 +1,76 @@
+//DIGITAL CLOCK FUNCTION-
+
+const Digital_Clock = () => {
+  const time =new Date();
+  let hrs = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  
+  let meridiem = document.getElementById('meridiem');
+  let hour = hrs;
+  let ampm = (hour >= 12) ? ('PM') : ('AM');
+  if (hrs >=12){
+    hrs = hrs-12;
+  }
+  if(hrs == 0){
+    hrs = 12;
+  }
+  meridiem.innerText= ampm;
+   
+  document.getElementById('hour').innerText = hrs;
+  document.getElementById('minutes').innerText = min;
+  document.getElementById('seconds').innerText = sec;
+   
+}
+setInterval(Digital_Clock,1000);
+
+//SET ALARM BUTTON REPLACING TEXTS-
+let button = document.getElementById('btn1');
+button.addEventListener('click', () => {
+    button.innerText = 'Party Time !!';
+   
+});
+button.addEventListener('mouseleave', () => {
+    button.innerText = 'Set Alarm';
+});
+
+//AUTOMATIC GRETTINGS-
+const date = new Date();
+let hrs = date.getHours();
+if (hrs <= 12) 
+{
+  document.getElementById("morning-label-id").innerHTML = "Good Morning!";
+  document.getElementById("image_photo").src = "./curtons-img.svg";
+  document.getElementById("breakfast-label-id").innerHTML ="GRAB SOME HEALTHY BREAKFAST!!!";
+}
+if (hrs >= 12 && hrs < 16) 
+{
+  document.getElementById("morning-label-id").innerHTML = "Good Afternoon!";
+  document.getElementById("image_photo").src = "./pizza.svg"
+  document.getElementById("breakfast-label-id").innerHTML ="LETS HAVE SOME LUNCH !!";
+
+}
+if (hrs >= 16 && hrs < 19)
+ {
+  document.getElementById("morning-label-id").innerHTML = "Good Evening";
+  document.getElementById("image_photo").src = "./lunch_image.png"
+  document.getElementById("breakfast-label-id").innerHTML ="GET SOME TEA";
+}
+if (hrs >= 19 && hrs <= 23) 
+{
+  document.getElementById("morning-label-id").innerHTML = "Good Night";
+  document.getElementById("image_photo").src = "./goodnight_image.svg"
+  document.getElementById("breakfast-label-id").innerHTML = "TIME TO BED";
+}
+
+//AFTER BUTTON PRESSED/SETTING ALARM-
+function clicked(){
+  
+  //TO SET TIMING-
+  let timing = document.getElementsByClassName("sets");
+  timing[0].innerText ="Wake Up Time:" + " " + wake.options[wake.selectedIndex].text;
+  timing[1].innerText ="Lunch Time:" + " " + lunch.options[lunch.selectedIndex].text;
+  timing[2].innerText ="Nap Time:" + " " + nap.options[nap.selectedIndex].text;
+  timing[3].innerText ="Night Time:" + " " + night.options[night.selectedIndex].text;
+
+ }
